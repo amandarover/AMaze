f41c9d41902b66bec0c10f35cb4a435acc7a59bc
amandarover <rover.amanda@gmail.com>
2017-06-11 23:38:22 -0300

    Add a menu to choose the maze mode

--- src/labirinto/main.java
+++ src/labirinto/main.java
@@ -1,10 +1,23 @@
 package labirinto;
 
+import javax.swing.JOptionPane;
+
 public class main {
     
     public static void main(String[] args) {
         Labirinto labirinto = new Labirinto();
+        String mode = JOptionPane.showInputDialog("Escolha tipo de labirinto:\n"
+                + "\nDigite 1 : busca em Profundidade"
+                + "\nDigite 2 : busca em Recursividade"
+                + "\nDigite 3 : busca em Amplitude (DEMO)");
         
-        labirinto.buscarPorLarguraRecursividade();
+        switch (mode) {
+            case "1": labirinto.buscarPorLargura();
+                break;
+            case "2": labirinto.buscarPorLarguraRecursividade();
+                break;
+            case "3": 
+                break;
+        }
     }
 }
